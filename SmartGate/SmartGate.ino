ee22ce634e28a3ba1fdbccdb14900b8fb9cf7210
edwardpassagi <edward.passagi@gmail.com>
2021-12-08 23:13:18 -0600

    refactored and modularized code

--- SmartGate/SmartGate.ino
+++ SmartGate/SmartGate.ino
@@ -4,6 +4,9 @@
 
 #define SS_PIN 4  //D2
 #define RST_PIN 5 //D1
+#define SD2 11
+#define SD3 12
+#define allowLCD true //enable lcd
 
 #include <SPI.h>
 #include <MFRC522.h>
@@ -16,30 +19,56 @@ LiquidCrystal lcd(RS, EN, d4, d5, d6, d7); // Create LED panel instance
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 int statuss = 0;
 int out = 0;
+
+
 void setup() 
 {
-  Serial.begin(9600);   // Initiate a serial communication
-  SPI.begin();      // Initiate  SPI bus
-  mfrc522.PCD_Init();   // Initiate MFRC522
-  lcd.begin(16,2); // Initiate LCD panel
+  initializer();
   Serial.println("Setting up serial output...");
 }
+
 void loop() 
 {
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
-  {
-    return;
-  }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  // Look for new cards or select one of the cards
+  if ( !mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) 
   {
     return;
   }
   //Show UID on serial monitor
+  String content= getContent();
+
+  // TODO: make HTTP get call to validate entry
+  bool isPermitted = validateContent(content);
+  if (isPermitted) //change UID of the card that you want to give access
+  {
+    authorizedSequence();
+  }
+  else   {
+    rejectedSequence();
+  }
+}
+
+/*
+ * This script is responsible to initialize all of our modules and IOs.
+ */
+void initializer()
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+
+  // this line overrides our serial monitor output!
+  if (allowLCD) lcd.begin(16,2); // Initiate LCD panel
+}
+
+/*
+ * getContent returns the UID of the current card being read.
+ */
+String getContent()
+{ 
+  String content = "";
   Serial.println();
   Serial.print(" UID tag :");
-  String content= "";
   byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
@@ -50,35 +79,51 @@ void loop()
   }
   content.toUpperCase();
   Serial.println();
+  return content;
+}
 
-
-  // make HTTP get call to validate entry
-  if (content.substring(1) == "7A E7 FD 3F") //change UID of the card that you want to give access
-  {
-    Serial.println(" Access Granted ");
-    Serial.println(" Welcome Mr.Circuit ");
-    
-    // print on lcd
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Access Granted");
-    lcd.setCursor(0, 1);
-    lcd.print("Welcome, Mo!");
-
-    delay(1000);
-    Serial.println(" Have FUN ");
-    Serial.println();
-    
-    statuss = 1;
+/**
+ * validateContent() makes determines wether current UID exists in our database or not.
+ */
+bool validateContent(String content)
+{
+  //TODO: add HTTP get call to validate with database
+  if (content.substring(1) == "7A E7 FD 3F"){
+    return true;
   }
+
+  return false;
+}
+
+/*
+ * Sequence upon succesful authorization.
+ */
+void authorizedSequence()
+{
+  Serial.println(" Access Granted ");
+  Serial.println(" Welcome Mr.Circuit ");
   
-  else   {
-    Serial.println(" Access Denied ");
+  // print on lcd
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Access Granted");
+  lcd.setCursor(0, 1);
+  lcd.print("Welcome, Mo!");
 
-    // print on lcd
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Access Denied!");
-    delay(3000);
-  }
-} 
+  delay(1000);
+  statuss = 1;
+}
+
+/*
+ * Sequence upon rejection.
+ */
+void rejectedSequence()
+{
+  Serial.println(" Access Denied ");
+  
+  // print on lcd
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Access Denied!");
+  delay(3000);
+}
